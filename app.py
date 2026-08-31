196479ec654b5c7e8172a90b2d71c42b4d443dc1
Sire65 <ha-joko@web.de>
2026-08-31 17:30:08 +0200

    Wire gated legacy SSH profile into NAS network diagnostics

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from update_ui import enable_auto_update, schedule_startup_update_check
 from cloud_failback import enable_cloud_failback
 from provider_registry import enable_provider_registry
 from nas_recovery.ui import NasRecoveryWindow, enable_nas_recovery
+from nas_recovery.network_ui import NasNetworkWindow
 from nas_recovery.raid_ui_integration import enable_raid_workspace
+from nas_recovery.ssh_legacy_ui_integration import enable_legacy_ssh_probe
 from nas_recovery.workflow_ui import enable_nas_workflow_ui
 from control_center import enable_control_center
 from nas_recovery.control_center_bridge import enable_nas_control_center_bridge
@@ -19,6 +21,7 @@ enable_auto_update(App)
 enable_cloud_failback(App)
 enable_provider_registry(App)
 enable_raid_workspace(NasRecoveryWindow)
+enable_legacy_ssh_probe(NasNetworkWindow)
 enable_nas_workflow_ui(NasRecoveryWindow)
 enable_nas_recovery(App)
 enable_nas_control_center_bridge()
