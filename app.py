60399bca4baa03aa8168a382cdd18839c35cf8cf
Sire65 <ha-joko@web.de>
2026-08-31 15:22:52 +0200

    Enable zero-cost cloud failback at application startup

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from plan_runner import run_plan
 from kicc_backup_telemetry import start_backup_telemetry
 from project_finder.main_integration import enable_project_finder
 from update_ui import enable_auto_update, schedule_startup_update_check
+from cloud_failback import enable_cloud_failback
 
 
 enable_project_finder(App)
 enable_auto_update(App)
+enable_cloud_failback(App)
 
 
 def _show_already_running():
