142d0dd98661b498a70a38b07e9d34eaa49d9521
Sire65 <ha-joko@web.de>
2026-09-07 20:28:17 +0200

    Enable HiDrive SFTP integration

--- app.py
+++ app.py
@@ -26,7 +26,10 @@ from test_window_usability_v184 import apply_test_window_usability_v184
 from restore_assistant_v186 import apply_restore_assistant_v186, RestoreAssistant, _file_group, _fmt_size
 from restore_path_fix_v188 import apply_restore_path_fix_v188
 from restore_filters_v189 import apply_restore_filters_v189
+import cloud_targets_v191 as cloud_targets_module
+import cloud_targets_ui_v191 as cloud_targets_ui_module
 from cloud_targets_ui_v191 import apply_cloud_targets_v191
+from hidrive_integration_v192 import apply_hidrive_sftp_v192
 from kc_backup_bridge_v180 import start_bridge
 import ui as ui_module
 
@@ -39,6 +42,11 @@ apply_settings_v180(SettingsWindow)
 apply_transfer_monitor(App)
 apply_assistant_v180(App)
 enable_inventory_backup_handoff(App, BackupAssistant)
+apply_hidrive_sftp_v192(cloud_targets_module, storage_v180_module)
+# cloud_targets_ui_v191 imported helpers by name; point them at the completed SFTP implementations.
+cloud_targets_ui_module.test_cloud_account = cloud_targets_module.test_cloud_account
+cloud_targets_ui_module.filesystem_capable_method = cloud_targets_module.filesystem_capable_method
+cloud_targets_ui_module.ensure_filesystem_bridge = cloud_targets_module.ensure_filesystem_bridge
 apply_cloud_targets_v191(SettingsWindow, BackupAssistant, storage_v180_module)
 apply_professional_v180(App, BackupAssistant, SettingsWindow, ui_module)
 apply_system_image_assistant(BackupAssistant)
