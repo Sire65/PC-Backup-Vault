93c9fe3a29129e941e6397980da77ba48b49c934
Sire65 <ha-joko@web.de>
2026-09-08 17:08:15 +0200

    Wire backup workbench without replacing backup engines

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ import hidrive_integration_v192 as hidrive_integration_module
 from hidrive_integration_v192 import apply_hidrive_sftp_v192
 from cloud_target_activation_v192 import apply_cloud_target_activation_v192
 from unified_reporting_integration_v193 import apply_unified_reporting_v193
+from backup_workbench_v194 import apply_backup_workbench_v194
 from kc_backup_bridge_v180 import start_bridge
 import ui as ui_module
 
@@ -61,6 +62,7 @@ apply_test_window_usability_v184(App)
 apply_restore_assistant_v186(App, BackupAssistant)
 apply_restore_path_fix_v188()
 apply_restore_filters_v189(RestoreAssistant, _file_group, _fmt_size)
+apply_backup_workbench_v194(App, storage_v180_module)
 
 
 def _show_already_running():
